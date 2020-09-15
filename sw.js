42c8cef218d9585a549f7933b440647642aa0ddc
sarthakforwet <sarthakkhandelwal032000@gmail.com>
2020-09-15 13:42:03 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-650faf0a0f9d81809bf4.js"
+    "url": "webpack-runtime-0b2d173d164ae6b604c7.js"
   },
   {
     "url": "framework-5fc6169171e885c3b522.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "86eecfe0550454db4baf91185f6b93bb"
+    "revision": "d5c9b7f22465bed2bf30100efaf94178"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "789bbb0bbad34da26c876097aaf1aa5e"
+    "revision": "a88e13d1dd052f368f18004cafeba621"
   },
   {
     "url": "manifest.webmanifest",
